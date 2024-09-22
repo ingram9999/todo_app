d9834c47a55231447570a90677aa4c6d01fbbcfc
Tsvetan <ttsankov2@gmail.com>
2024-09-22 23:12:24 +0300

    98_final

--- lib/2_application/core/routes.dart
+++ lib/2_application/core/routes.dart
@@ -2,41 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/2_application/core/go_router_observer.dart';
 import 'package:todo_app/2_application/pages/home/home_page.dart';
+import 'package:todo_app/2_application/pages/settings/settings_page.dart';
 
-final GlobalKey<NavigatorState> _rootNavigatorKey =
-    GlobalKey<NavigatorState>(debugLabel: 'root');
-final GlobalKey<NavigatorState> _shellNavigatorKey =
-    GlobalKey<NavigatorState>(debugLabel: 'shell');
+final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 final routes = GoRouter(
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/home',
+  initialLocation: '/home/dashboard',
   observers: [GoRouterObserver()],
   routes: [
     GoRoute(
       path: '/home/settings',
       builder: (context, state) {
-        return Container(
-          color: Colors.amber,
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () => context.push('/home/start'),
-                child: const Text('Go to start'),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (context.canPop()) {
-                    context.pop();
-                  } else {
-                    context.push('/home/start');
-                  }
-                },
-                child: const Text('Go back'),
-              ),
-            ],
-          ),
-        );
+        return const SettingsPage();
       },
     ),
     ShellRoute(
@@ -46,7 +25,7 @@ final routes = GoRouter(
         GoRoute(
           path: '/home/:tab',
           builder: (context, state) => HomePage(
-            key: state.pageKey, 
+            key: state.pageKey,
             tab: state.pathParameters['tab'] ?? 'dashboard',
           ),
         ),
